151a52468c73bae17190b7f8b3318a573059ab14
HF <2625914411@qq.com>
2025-04-03 21:15:29 +0800

    add WordCount.sh

---
+++ WordCount.sh
@@ -0,0 +1,6 @@
+## make sure to run this script in the root directory of the project
+# 统计 .cc 文件行数
+find ./ -name "*.cc" -exec wc -l {} + | awk '{sum += $1} END {print "CC:", sum}'
+
+# 统计 .h 文件行数
+find ./ -name "*.h" -exec wc -l {} + | awk '{sum += $1} END {print "H:", sum}'
